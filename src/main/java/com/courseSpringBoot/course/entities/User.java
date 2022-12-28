068797f0e90c41a272fa485aaa30f31cd409eee5
Bruno Domingos Teixeira <bruninhodt2467@gmail.com>
2022-12-28 16:26:05 -0300

    Order class part 1

--- src/main/java/com/courseSpringBoot/course/entities/User.java
+++ src/main/java/com/courseSpringBoot/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.courseSpringBoot.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List <Order> orders = new ArrayList<>();
+	
 	public User() {}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -33,33 +39,48 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	
 	public Long getId() {
 		return id;
 	}
 
-
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
 	}
 
-
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public String getEmail() {
 		return email;
 	}
 
-
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 	public String getPhone() {
 		return phone;
 	}
 
-
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
 	public String getPassword() {
 		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
@@ -79,10 +100,4 @@ public class User implements Serializable {
 		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name);
 	}
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password
-				+ "]";
-	}
-	
 }
