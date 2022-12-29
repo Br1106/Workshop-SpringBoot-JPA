e40dd62b24298d2163482fa3a4d8a00f702ff4e3
Bruno Domingos Teixeira <bruninhodt2467@gmail.com>
2022-12-29 15:12:51 -0300

    Category entity, Repository,Service & Resource

---
+++ src/main/java/com/courseSpringBoot/course/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.courseSpringBoot.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.courseSpringBoot.course.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
